e50972cb95cf53ca08fd27fa21149b059509de0c
SaQie <Kamcio99@interia.com>
2025-12-23 10:06:35 +0100

    added keycloak config

---
+++ src/main/java/com/github/saqie/api/auth/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.github.saqie.api.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex
+                        .pathMatchers("/actuator/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> {
+                }))
+                .build();
+    }
+
+}
